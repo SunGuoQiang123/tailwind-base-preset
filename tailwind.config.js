e91eb5d981c679a2b0a4686b61c5279ef8214ba8
sunguoqiang04 <sunguoqiang04@meituan.com>
2022-05-13 01:46:20 +0800

    feat: first co

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  theme: {
+    spacing: {
+      '1': '12px',
+      '2': '14px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    },
+    extend: {
+      colors: {
+        primary: '#1890FF',
+        success: '#52c41a',
+        warning: '#faad14',
+        danger: '#ff4d4f',
+        primaryText: 'rgba(0, 0, 0, 0.85)',
+        secondaryText: 'rgba(0, 0, 0, 0.45)'
+      },
+    }
+  }
+}
