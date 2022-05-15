364781d8ae1a81e4cf6032f1a4d428d1406063aa
sunguoqiang04 <sunguoqiang04@meituan.com>
2022-05-15 12:35:55 +0800

    feat: add fontSize

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,43 @@
 module.exports = {
   theme: {
     spacing: {
+      'px': '1px',
+      '0': '0px',
       '1': '12px',
       '2': '14px',
       '3': '16px',
       '4': '24px',
       '5': '32px',
       '6': '48px',
+      '7': '60px',
+      '8': '72px',
+      '9': '96px',
+      '10': '108px',
+      '11': '120px',
+      '12': '140px',
+      '13': '160px',
+      '14': '200px',
+      '15': '240px',
+      '16': '300px',
+      '17': '360px',
+      '18': '400px',
+      '19': '460px',
+      '20': '500px'
+    },
+    fontSize: {
+      xs: ['12px', { lineHeight: '16px' }],
+      sm: ['14px', { lineHeight: '20px' }],
+      base: ['16px', { lineHeight: '24px' }],
+      lg: ['18px', { lineHeight: '28px' }],
+      xl: ['20px', { lineHeight: '28px' }],
+      '2xl': ['24px', { lineHeight: '32px' }],
+      '3xl': ['30px', { lineHeight: '36px' }],
+      '4xl': ['36px', { lineHeight: '40px' }],
+      '5xl': ['48px', { lineHeight: '1' }],
+      '6xl': ['60px', { lineHeight: '1' }],
+      '7xl': ['72px', { lineHeight: '1' }],
+      '8xl': ['96px', { lineHeight: '1' }],
+      '9xl': ['128px', { lineHeight: '1' }],
     },
     extend: {
       colors: {
